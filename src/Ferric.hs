c4972fbd6019d9aaf8492eacab88523506ff4e78
Will Dey <wdey@palantir.com>
2026-08-21 13:30:32 +0100

    more

--- src/Ferric.hs
+++ src/Ferric.hs
@@ -1,2 +1,13 @@
-module Ferric where
+module Ferric (crate) where
 
+import Language.Haskell.TH
+import Network.HTTP.Conduit
+import qualified Codec.Compression.Zstd.Lazy as Zstd
+import qualified Data.Aeson as Aeson
+
+crate :: String -> String -> Q [Dec]
+crate name version = do
+  compressed <- runIO $ simpleHttp $ "https://docs.rs/crate/" ++ name ++ "/" ++ version ++ "/json"
+  let decompressed = Aeson.decode @Aeson.Value $ Zstd.decompress compressed
+  runIO $ putStrLn $ show decompressed
+  return []
