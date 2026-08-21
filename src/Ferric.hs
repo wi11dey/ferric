13248fbe28b54c6511a79d1e893b47964d3d5fdf
Will Dey <wdey@palantir.com>
2026-08-21 16:21:33 +0100

    more

--- src/Ferric.hs
+++ src/Ferric.hs
@@ -4,13 +4,15 @@ import qualified Codec.Compression.Zstd.Lazy as Zstd
 import Data.Aeson
 import Data.ByteString.Lazy (ByteString)
 import Data.Map.Strict (Map, (!))
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy.IO as Text
 import Ferric.RustDoc.Crate
 import Ferric.RustDoc.Item
 import qualified Ferric.RustDoc.ItemEnum as ItemEnum
-import Text.Pretty.Simple
 import Ferric.RustDoc.Module
 import Language.Haskell.TH
 import Network.HTTP.Conduit
+import Text.Pretty.Simple
 
 crate :: String -> String -> Q [Dec]
 crate name version = do
@@ -21,15 +23,15 @@ crate' :: ByteString -> Q [Dec]
 crate' rustdocJson = do
   Crate {..} <- throwDecode @Crate rustdocJson
   runIO $ do
-    mapM_ pPrint $ emit index root
+    mapM_ Text.putStrLn $ emit index root
   return []
 
-emit :: Map Int Item -> Int -> [String]
+emit :: Map Int Item -> Int -> [Text]
 emit index root =
-  let Item {name, inner} = index ! root
+  let Item {inner} = index ! root
    in case inner of
         ItemEnum.Module Module {..} -> concatMap (emit index) items -- TODO: make new module
-        ItemEnum.Enum enum -> [show enum]
-        ItemEnum.Struct struct -> [show struct]
-        ItemEnum.Impl impl -> [show impl]
+        ItemEnum.Enum enum -> [pShow enum]
+        ItemEnum.Struct struct -> [pShow struct]
+        ItemEnum.Impl impl -> [pShow impl]
         _ -> []
